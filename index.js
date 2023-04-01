5642cb478c83dc3f4b93a7bbd3e61d27f68f8a27
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-04-01 12:31:52 +0600

    code commet added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const stripe = require("stripe")(
   "sk_test_51L6u7WFgsutIdwUumMVOPUwYY59uIRyXwS3QKLg7Prb1oG5X7FLsGcfXBAXYcgdCcHIAXvozu7WSWWcAZjCgtEXa00ORWdF8pa"
 );
 
+// port 
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -16,8 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
+//uri link
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqjnl.mongodb.net/?retryWrites=true&w=majority`;
 
+//mongoclient connection
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
